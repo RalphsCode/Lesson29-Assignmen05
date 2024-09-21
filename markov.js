e4457db7c51f0096016727c4baa9144debe4086f
RalphsCode <RalphsCode@gmail.com>
2024-09-21 10:49:41 -0700

    makeChains to construct map of chains of word → possible-next-words, functional

--- markov.js
+++ markov.js
@@ -8,7 +8,8 @@ class MarkovMachine {
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
     this.words = words.filter(c => c !== "");
-    this.makeChains();
+    console.log("### this.words:", this.words)
+    this.makeChains(this.words);
   }
 
   /** set markov chains:
@@ -16,8 +17,25 @@ class MarkovMachine {
    *  for text of "the cat in the hat", chains will be
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
-  makeChains() {
-    // TODO
+  makeChains(text) {
+    // Argument: [ 'the', 'cat', 'in', 'the', 'hat' ]
+    console.log("### text argument:", text);
+    let word_dict = {};
+    
+    for (let i=0; i<text.length; i++) {
+      let word = text[i];
+      let next_word = text[i+1];
+      if (next_word == undefined) {
+        next_word = null;
+      }
+      if (word_dict[word]) {
+        word_dict[word].push(next_word);
+      } else {
+        word_dict[word] = [next_word];
+      }
+      console.log("### word_dict:", word_dict);
+      }
+      return word_dict;
   }
 
 
@@ -27,3 +45,8 @@ class MarkovMachine {
     // TODO
   }
 }
+
+words = new MarkovMachine("the cat in the hat is in the hat");
+
+
+
